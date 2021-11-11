fef841cc8d736a6ef8e17cbcd2dd21743a230fe7
Mihail Alekseev <mihailalekseev02@gmail.com>
2021-11-11 10:21:48 +0200

    Initial commit with preparing

---
+++ EFAccessDataLibrary/Models/Dishe.cs
@@ -0,0 +1,23 @@
+﻿namespace EFDataAccessLibrary.Models;
+
+public class Dishe
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int? Calories { get; set; }
+
+    public int? Proteins { get; set; }
+
+    public int? Fats { get; set; }
+
+    public int? Carbs { get; set; }
+
+    public List<Ingredient> Ingredients { get; set; }
+
+    public Dishe()
+    {
+
+    }
+}
---
+++ EFAccessDataLibrary/RestaurantContext.cs
@@ -0,0 +1,29 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDataAccessLibrary;
+
+public class RestaurantContext : DbContext
+{
+    public RestaurantContext()
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
+    public RestaurantContext(DbContextOptions options) : base(options) { }
+
+    public virtual DbSet<Ingredient> Ingredients { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    void OnModelCreatingPartial(ModelBuilder modelBuilder)
+    {
+
+    }
+}
